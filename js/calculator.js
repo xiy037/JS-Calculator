cc52c519c9e00eb0dd310c2ae0336df67152c7ae
Xiao Yu <xiao.susan.yu@gmail.com>
2019-11-15 11:11:21 +0800

    seperate number and operator input to make code easier to read

--- js/calculator.js
+++ js/calculator.js
@@ -1,40 +1,58 @@
-var result = document.getElementById("result");
-var calResult;
+var result = document.getElementsByClassName("result")[0];
+var calResult = "";
 var tempString = "";
+
 function clr() {
     result.value="";
-    calResult= "a";
+    calResult= "";
     tempString = "";
 }
-function display(clickVal) {
-    var operatorCheck = '+-*/.';
-    var pos = tempString.length -1;
-    var lastValue = tempString.substr(pos);
-    var checkClickVal = operatorCheck.indexOf(clickVal);
-    var checkLastValue = operatorCheck.indexOf(lastValue);
-    if ((checkClickVal !== -1) && (checkLastValue !== -1)) {
-        return tempString;
-    } else if ((checkClickVal == -1)&& (tempString == calResult)) {
-        tempString = '';
-        tempString +=clickVal;
-    } else {
-        tempString +=clickVal;
-    } 
-    result.value = tempString;
-    if ((lastValue == '/') && (clickVal == '0')) {
-        result.value = "Error";
+function buttonNumber(btnVal) {
+    var position = tempString.length -1;
+    var lastValue = tempString.substr(position);
+    //下一次轮输入数字时清空上一轮结果。
+    if ((tempString == calResult) && (calResult !== "")) {
+        tempString = "";
+        tempString += btnVal;
+    } else if ((lastValue == '/') && (btnVal == '0')) {
+    //被除数不能为零。
+        tempString += btnVal;
+        calResult = "Error";
     }
+    else {
+        tempString += btnVal;
+    }
+    result.value = tempString;  
+}
+function buttonOperator(btnVal) {   
+    var position = tempString.length -1;
+    var lastValue = tempString.substr(position);
+    var operatorCheck = "+-*/.";
+    var checkLastValue = operatorCheck.indexOf(lastValue);
+    //不连续输入两个运算符。
+    if (checkLastValue !== -1) {
+        return tempString;
+    } else {
+        tempString += btnVal;
+    }
+    result.value = tempString;
 }
 function opposite() {
     tempString = -1*tempString + '';
+    calResult = tempString;
     result.value = tempString;
 }
 function percent() {
     tempString = tempString/100 + '';
+    calResult = tempString;
     result.value = tempString;
 }
 function calculate() {
-    calResult = eval(tempString);
-    result.value = calResult;
+    if (calResult !== "Error") {
+        calResult = eval(tempString);
+        result.value = Math.round(calResult*1e10)/1e10;
+    } else {
+        result.value = calResult;
+    }  
     tempString = calResult + '';
 }
